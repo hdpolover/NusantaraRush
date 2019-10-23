2dc1549d045c2f844302e891b7ee99de8e7a29b6
Bagus Aditama <aditoye105@gmail.com>
2019-10-23 17:52:25 +0700

    Credit screen menu

---
+++ Assets/Scripts/Strategy/Credit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    public GameObject creditScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditScreen.SetActive(false);    
+    }
+
+    public void EnableCreditScreen()
+    {
+        creditScreen.SetActive(true);    
+    }
+
+    public void DisableCreditScreen()
+    {
+        creditScreen.SetActive(false);
+    }
+}
